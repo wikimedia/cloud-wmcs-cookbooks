311bb0e250e0c977831abd13a5043296018f3d90
David Caro <dcaro@wikimedia.org>
2023-05-15 23:20:56 +0200

    toolforge.k8s.reboot: force reboot when stuck
    Also allow rebooting only the worker nodes.
    
    Signed-off-by: David Caro <dcaro@wikimedia.org>
    Change-Id: Icabdecc3158a17a8a7f0b4687cf37f2536a8ebbe

--- cookbooks/wmcs/toolforge/k8s/reboot.py
+++ cookbooks/wmcs/toolforge/k8s/reboot.py
@@ -26,6 +26,7 @@ from wmcs_libs.k8s.clusters import (
     with_toolforge_kubernetes_cluster_opts,
 )
 from wmcs_libs.k8s.kubernetes import KubernetesController
+from wmcs_libs.openstack.common import OpenstackAPI, OpenstackClusterName
 
 LOGGER = logging.getLogger(__name__)
 
@@ -56,6 +57,12 @@ class ToolforgeK8sReboot(CookbookBase):
             action="store_true",
             help="operate on all cluster nodes",
         )
+        parser.add_argument(
+            "--all-workers",
+            required=False,
+            action="store_true",
+            help="operate on all cluster worker nodes",
+        )
         return parser
 
     def get_runner(self, args: argparse.Namespace) -> WMCSCookbookRunnerBase:
@@ -64,6 +71,7 @@ class ToolforgeK8sReboot(CookbookBase):
             spicerack=self.spicerack,
             hostname_list=args.hostname_list,
             do_all=args.all,
+            do_all_workers=args.all_workers,
         )
 
 
@@ -76,24 +84,31 @@ class ToolforgeK8sRebootRunner(WMCSCookbookRunnerBase):
         cluster_name: ToolforgeKubernetesClusterName,
         hostname_list: list[str],
         do_all: bool,
+        do_all_workers: bool,
         spicerack: Spicerack,
-    ):
+    ):  # pylint: disable=too-many-arguments
         """Init"""
         super().__init__(spicerack=spicerack)
         self.common_opts = common_opts
         self.cluster_name = cluster_name
         self.hostname_list = hostname_list
         self.do_all = do_all
+        self.do_all_workers = do_all_workers
         self.domain = f"{self.common_opts.project}.eqiad1.wikimedia.cloud"
         self.sallogger = SALLogger(
             project=common_opts.project, task_id=common_opts.task_id, dry_run=common_opts.no_dologmsg
         )
+        self.openstack_api = OpenstackAPI(
+            remote=spicerack.remote(),
+            cluster_name=OpenstackClusterName.EQIAD1,
+            project=self.common_opts.project,
+        )
 
-        if do_all and hostname_list:
-            raise Exception("--all and --hostname-list are mutually exclusive")
+        if (do_all or do_all_workers) and hostname_list:
+            raise Exception("--all/--all-workers and --hostname-list are mutually exclusive")
 
-        if not do_all and not hostname_list:
-            raise Exception("either --all or --hostname-list needs to be specified")
+        if not do_all and not hostname_list and not do_all_workers:
+            raise Exception("either --all, --all-workers or --hostname-list needs to be specified")
 
     def _select_k8s_controller(self) -> None:
         """Select a k8s control node."""
@@ -104,15 +119,31 @@ class ToolforgeK8sRebootRunner(WMCSCookbookRunnerBase):
         k8s_controller = KubernetesController(self.spicerack.remote(), control_node_fqdn)
         LOGGER.info("INFO: using control node %s", control_node_fqdn)
 
-        if self.do_all:
+        if self.do_all or self.do_all_workers:
             # in reverse order, so the controllers are done last!
             self.hostname_list = k8s_controller.get_nodes_hostnames()[::-1]
+            if not self.do_all_workers:
+                self.sallogger.log(
+                    f"rebooting the whole {self.common_opts.project} k8s cluster ({len(self.hostname_list)} nodes)"
+                )
+
+        if self.do_all_workers:
+            self.hostname_list = [node for node in self.hostname_list if "-worker-" in node]
             self.sallogger.log(
-                f"rebooting the whole {self.common_opts.project} k8s cluster ({len(self.hostname_list)} nodes)"
+                f"rebooting all the workers of {self.common_opts.project} k8s cluster ({len(self.hostname_list)} nodes)"
             )
 
         for node_hostname in self.hostname_list:
-            for phase in k8s_controller.reboot_node(node_hostname, self.domain):
-                LOGGER.info("INFO: %s: reboot phase: %s", node_hostname, phase)
+            try:
+                for phase in k8s_controller.reboot_node(node_hostname, self.domain):
+                    LOGGER.info("INFO: %s: reboot phase: %s", node_hostname, phase)
+            except Exception:  # pylint: disable=broad-except
+                LOGGER.info(
+                    "Something happened while rebooting host %s, trying a hard rebooting the instance",
+                    node_hostname,
+                )
+                self.openstack_api.server_force_reboot(node_hostname)
+                k8s_controller.uncordon_node(node_hostname)
+                k8s_controller.wait_for_ready(node_hostname)
 
             self.sallogger.log(f"rebooted k8s node {node_hostname}")
