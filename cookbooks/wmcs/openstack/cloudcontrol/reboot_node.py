cd3283e26b42b5fbf72e8884945dd9fc927c44be
Taavi Väänänen <hi@taavi.wtf>
2024-03-26 12:21:35 +0200

    openstack: cloudcontrol: Convert reboot cookbook to batch base
    Change-Id: I8dbf958e8391d2ee30c04d1634b675b1dde51291

--- cookbooks/wmcs/openstack/cloudcontrol/reboot_node.py
+++ cookbooks/wmcs/openstack/cloudcontrol/reboot_node.py
@@ -2,7 +2,7 @@ r"""WMCS Openstack - Reboot a cloudcontrol node .
 
 Usage example:
     cookbook wmcs.openstack.cloudcontrol.reboot_node \
-    --fqdn-to-reboot cloudcontrol1005.eqiad.wmnet
+        --fqdn cloudcontrol1005.eqiad.wmnet
 
 """
 from __future__ import annotations
@@ -11,89 +11,34 @@ import argparse
 import logging
 from datetime import datetime
 
-from spicerack import Spicerack
-from spicerack.cookbook import ArgparseFormatter, CookbookBase
+from spicerack import RemoteHosts
 
-from wmcs_libs.alerts import downtime_host, uptime_host
-from wmcs_libs.common import CommonOpts, SALLogger, WMCSCookbookRunnerBase, add_common_opts, with_common_opts
-from wmcs_libs.openstack.common import get_control_nodes, get_node_cluster_name
+from wmcs_libs.common import WMCSCookbookRunnerBase, with_common_opts
+from wmcs_libs.openstack.batch import CloudcontrolBatchBase, CloudcontrolBatchRunnerBase
 
 LOGGER = logging.getLogger(__name__)
 
 
-class RebootNode(CookbookBase):
+class RebootNode(CloudcontrolBatchBase):
     """WMCS Openstack cookbook to reboot a single cloudcontrols, handling failover."""
 
     title = __doc__
 
-    def argument_parser(self):
-        """Parse the command line arguments for this cookbook."""
-        parser = argparse.ArgumentParser(
-            prog=__name__,
-            description=__doc__,
-            formatter_class=ArgparseFormatter,
-        )
-        add_common_opts(parser)
-        parser.add_argument(
-            "--fqdn-to-reboot",
-            required=True,
-            help="FQDN of the node to reboot.",
-        )
-
-        return parser
-
     def get_runner(self, args: argparse.Namespace) -> WMCSCookbookRunnerBase:
         """Get runner"""
         return with_common_opts(self.spicerack, args, RebootNodeRunner,)(
-            fqdn_to_reboot=args.fqdn_to_reboot,
+            args=args,
             spicerack=self.spicerack,
         )
 
 
-class RebootNodeRunner(WMCSCookbookRunnerBase):
+class RebootNodeRunner(CloudcontrolBatchRunnerBase):
     """Runner for RebootNode"""
 
-    def __init__(
-        self,
-        common_opts: CommonOpts,
-        fqdn_to_reboot: str,
-        spicerack: Spicerack,
-    ):
-        """Init"""
-        self.common_opts = common_opts
-        self.fqdn_to_reboot = fqdn_to_reboot
-        super().__init__(spicerack=spicerack, common_opts=common_opts)
-        self.sallogger = SALLogger.from_common_opts(common_opts=common_opts)
+    downtime_reason = "host reboot"
 
-        self.cluster_name = get_node_cluster_name(self.fqdn_to_reboot)
-
-        known_cloudcontrols = get_control_nodes(cluster_name=self.cluster_name)
-        if not known_cloudcontrols:
-            raise Exception(f"No cloudcontrols found for cluster_name {self.cluster_name} :-S")
-
-        if self.fqdn_to_reboot not in known_cloudcontrols:
-            raise Exception(
-                f"Host {self.fqdn_to_reboot} is not part of the cloudcontrol for cluster_name {self.cluster_name}"
-            )
-
-    def run_with_proxy(self) -> None:
+    def run_on_hosts(self, hosts: RemoteHosts) -> None:
         """Main entry point"""
-        self.sallogger.log(f"Rebooting cloudcontrol host {self.fqdn_to_reboot}")
-        node = self.spicerack.remote().query(f"D{{{self.fqdn_to_reboot}}}", use_sudo=True)
-        host_name = self.fqdn_to_reboot.split(".", 1)[0]
-        host_silence_id = downtime_host(
-            spicerack=self.spicerack,
-            host_name=host_name,
-            comment="Rebooting with wmcs.openstack.cloudcontrol.reboot_node",
-            task_id=self.common_opts.task_id,
-        )
-
         reboot_time = datetime.utcnow()
-        node.reboot()
-
-        node.wait_reboot_since(since=reboot_time)
-
-        uptime_host(spicerack=self.spicerack, host_name=host_name, silence_id=host_silence_id)
-        LOGGER.info("Silences removed.")
-
-        self.sallogger.log(f"Rebooted cloudcontrol host {self.fqdn_to_reboot}")
+        hosts.reboot()
+        hosts.wait_reboot_since(since=reboot_time)
--- wmcs_libs/openstack/batch.py
+++ wmcs_libs/openstack/batch.py
@@ -13,6 +13,52 @@ from wmcs_libs.inventory.openstack import OpenstackClusterName
 from wmcs_libs.openstack.common import get_node_cluster_name
 
 
+class CloudcontrolBatchBase(CookbookBase, metaclass=ABCMeta):
+    """Base cookbook class for batch operations on clouducontrol nodes."""
+
+    def argument_parser(self) -> argparse.ArgumentParser:
+        parser = argparse.ArgumentParser(
+            prog=__name__,
+            description=__doc__,
+            formatter_class=ArgparseFormatter,
+        )
+        add_common_opts(parser)
+
+        parser.add_argument(
+            "--fqdn",
+            help="Operate on this specific node",
+        )
+        parser.add_argument(
+            "--cluster-name",
+            choices=list(OpenstackClusterName),
+            type=OpenstackClusterName,
+            help="Operate on all nodes of this cluster",
+        )
+
+        return parser
+
+
+class CloudcontrolBatchRunnerBase(WMCSCookbookBatchRunnerBase, metaclass=ABCMeta):
+    """Base cookbook runner class for batch operations on cloudvirt nodes."""
+
+    def __init__(
+        self,
+        common_opts: CommonOpts,
+        args: argparse.Namespace,
+        spicerack: Spicerack,
+    ):
+        """Init"""
+        super().__init__(common_opts, spicerack)
+        if args.fqdn:
+            self.cluster = get_node_cluster_name(args.fqdn)
+            self.query = f"D{{{args.fqdn}}}"
+        elif args.cluster_name:
+            self.cluster = args.cluster_name
+            self.query = f"P{{O:wmcs::openstack::{self.cluster.value}::control}}"
+        else:
+            raise ValueError("Either --fqdn or --cluster-name must be specified")
+
+
 class CloudvirtBatchBase(CookbookBase, metaclass=ABCMeta):
     """Base cookbook class for batch operations on cloudvirt nodes."""
 
