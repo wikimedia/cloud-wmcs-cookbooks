5ba5f4df9d38943ba82d926146ab2dc4ff3e7caf
Taavi Väänänen <hi@taavi.wtf>
2025-09-16 18:48:50 +0300

    aptly: Stop updating pre-Trixie repositories
    Bug: T404733
    Signed-off-by: David Caro <dcaro@wikimedia.org>
    Change-Id: If69972720761d90248d754424288f0f9cb4a2a1c

--- wmcs_libs/aptly.py
+++ wmcs_libs/aptly.py
@@ -6,7 +6,7 @@ from pathlib import Path
 
 from wmcs_libs.common import CUMIN_SAFE_WITH_OUTPUT, CUMIN_UNSAFE_WITHOUT_OUTPUT, CommandRunnerMixin
 
-SUPPORTED_DISTROS = ["buster", "bullseye", "bookworm", "trixie"]
+SUPPORTED_DISTROS = ["trixie"]
 
 
 class Aptly(CommandRunnerMixin):
